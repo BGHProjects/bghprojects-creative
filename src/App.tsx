a0bcbfded74c42ac21719772d1603727695dc28b
BGHProjects <un12known34_acc@outlook.com>
2023-05-01 17:56:28 +1000

    fix: :bug: should wait for fonts to be loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import Home from "./pages/Home";
 
-function App() {
-  return <Home />;
-}
+const App = () => {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const checkFontLoad = async () => {
+      const font = new FontFace(
+        "Open Sans",
+        'url("/assets/fonts/Electrolize/Electrolize-Regular.ttf")'
+      );
+      try {
+        await font.load();
+        document.fonts.add(font);
+        setIsFontLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    checkFontLoad();
+  }, []);
+
+  return isFontLoaded ? <Home /> : null;
+};
 
 export default App;
